f77dfb3b0757674b44d60b4e7e1bb02f2a9e2994
davros1973 <gitHub@xarta.co.uk>
2015-12-29 01:07:53 +0000

    ch5 Question 7

--- ch5_practice/ch5_practice.cpp
+++ ch5_practice/ch5_practice.cpp
@@ -3,10 +3,14 @@
 
 // http://www.amazon.co.uk/Jumping-into-C-Alex-Allain/dp/0988927802
 // Chapter 5
-// practice question 6
+// practice question 7
 
 /*
-Write a program that displays the first 20 square numbers.
+Write a program that provides the option of tallying up the results of a poll with 3 possible values. The
+first input to the program is the poll question; the next three inputs are the possible answers. The first
+answer is indicated by 1, the second by 2, the theird by 3. The answers are tallied until a 0 is entered. The 
+program should then show the results of the poll - try making a bar graph that shoes the results properly
+scaled to fit on your screen no matter how many results were entered.
 */
 
 
@@ -44,14 +48,187 @@ using namespace std;
 
 int main()
 {
-    // assuming just means 1 x 1, 2 x 2 etc. ... with 20 results ???
-    // seems a bit simple for this stage???
-    // not sure if "square number" means something different in which case doing this wrong
+    /*
+        Breaking down question:
+        
+        Poll:
+            Ask ONE question:
 
-    for (int numToSquare = 1; numToSquare <= 20; numToSquare++)
+            LOOP BEGIN ->
+            Input answer ...
+                0 terminate and show results incl. scaled bar-graph
+                1, or
+                2, or
+                3
+            LOOP BACK TO BEGIN
+
+        No functions
+        No arrays
+        No structures etc.
+
+        Can represent number of each answer, 1 or 2 or 3 by an integer each. Just a count.
+        
+        If no results, flat bar graph, 0.
+        If all results are the same then flat bar graph, max. height.
+        
+        Otherwise:
+        Then, take biggest (non-zero) result (or one of two results if same) and represent in a graph at a fixed height.
+        Then take the next biggest (or same magnitude) result, and scale to scale on used on screen
+        And then scale final result.
+
+    */
+
+    string question = "Which of these colours do you prefer?\n1. Orange, 2. Yellow, 3. Beige.\nAnswer 1, 2 or 3.";
+    int answercount1 = 0;
+    int answercount2 = 0;
+    int answercount3 = 0;
+    int answerbiggest = 0;
+    int answer = 0;
+
+    double scale1 = 0;
+    double scale2 = 0;
+    double scale3 = 0;
+
+    int bargraphMax = 25; // max. asci characters high for bargraph
+    int bar1 = 0;
+    int bar2 = 0;
+    int bar3 = 0;
+
+    do
     {
-        cout << numToSquare << "\tsquared = " << numToSquare * numToSquare << endl;
+        cout << question << endl;
+        cin >> answer;
+
+        // going to cheat and use a switch
+        switch ( answer )
+        {
+            case 0:
+                break;
+            case 1:
+                answercount1++;
+                break;
+            case 2:
+                answercount2++;
+                break;
+            case 3:
+                answercount3++;
+                break;
+            default:
+                cout << "Please only enter 1, or 2, or 3 ... or 0 to tally." << endl;
+                break;
+        }
+
+    } while (answer);
+    
+    if (answercount1 == answercount2 == answercount3)
+    {
+        if (answercount1 == 0)
+        {
+            bar1 = 0;
+            bar2 = 0;
+            bar3 = 0;
+        }
+        else
+        {
+            bar1 = bargraphMax;
+            bar2 = bargraphMax;
+            bar3 = bargraphMax;
+        }
     }
+    else
+    {
+        cout << "debug: answercount1 = " << answercount1 << endl;
+        cout << "debug: answercount2 = " << answercount2 << endl;
+        cout << "debug: answercount3 = " << answercount3 << endl;
+
+
+        // can't use an array yet for simple insertion swap etc.  :(
+        // could calculation max. iterations for a loop to sort answers ...
+        // though without structures etc. that's still a bit messy with nominals.
+        // but not doing that here.  Just find biggest result etc.
+
+        if (  answercount1 >= answercount2 && answercount2 >= answercount3  ||
+              answercount1 >= answercount3 && answercount3 >= answercount2  )
+        {
+            cout << "debug: answercount1 is biggest" << endl;
+            answerbiggest = answercount1;
+
+            scale1 = bargraphMax;
+            scale2 = answercount2 / (double)answercount1 * bargraphMax;
+            scale3 = answercount3 / (double)answercount1 * bargraphMax;
+        }
+        else if (  answercount2 >= answercount1 && answercount1 >= answercount3  ||
+                   answercount2 >= answercount3 && answercount3 >= answercount1  )
+        {
+            cout << "debug: answercount2 is biggest" << endl;
+            answerbiggest = answercount2;
+
+            scale2 = bargraphMax;
+            scale1 = answercount1 / (double)answercount2 * bargraphMax;
+            scale3 = answercount3 / (double)answercount2 * bargraphMax;
+
+        }
+        else
+        {
+            cout << "debug: answercount3 is biggest" << endl;
+            answerbiggest = answercount3;
+
+            scale3 = bargraphMax;
+            scale1 = answercount1 / (double)answercount3 * bargraphMax;
+            scale2 = answercount2 / (double)answercount3 * bargraphMax;
+        }
+
+        bar1 = scale1; // implict cast to int
+        bar2 = scale2;
+        bar3 = scale3;
+    }
+
+    // OUTPUT
+    
+    // make cartesian ordinal raster using x, y in nested For loop
+    // make bargraph columns 5 characters wide
+    // ... with 1 character spacing
+
+    // top  -> down
+    // left -> right (bar1, bar2, bar3)
+ 
+    cout << "bar1 = " << bar1 << endl;
+    cout << "bar2 = " << bar2 << endl;
+    cout << "bar3 = " << bar3 << endl;
+
+
+    for (int y = bargraphMax-1; y >= 0; y--)
+    {
+        for (int x = 0; x < ((3 * 5) + (1 * 2)); x++)
+        {
+            if (x < 5 || x > 5 && x < 11 || x > 11 && x < 17)
+            {
+                if ( x < 5  && bar1 > y ||
+                     x < 11 && bar2 > y ||
+                     x < 17 && bar3 > y)
+                {
+                    cout << "@";
+                }
+                else
+                {
+                    cout << ".";
+                }
+            }
+            else
+            {
+                cout << ".";
+            }
+        }
+        cout << endl;
+    }
+
+    if (answerbiggest < 100)
+    {
+        // approx. ... would format this more nicely using say, string conversion
+        //             and padding ... or more ifs / and padding.
+        cout << ".." << answercount1 << ". .." << answercount2 << ". .." << answercount3 << "." << endl;
+    }
+    
 
     MacroWaitReturn
 }
